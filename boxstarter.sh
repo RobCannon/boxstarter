4110796eb55cc6273c7fd12bd80b55ddbd9b3f4f
Cannon, Robert <Robert.Cannon@turner.com>
2018-08-03 21:20:58 -0400

    Fixing install for azure cli

--- boxstarter.sh
+++ boxstarter.sh
@@ -8,13 +8,14 @@ AZ_REPO=$(lsb_release -cs)
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | \
     sudo tee /etc/apt/sources.list.d/azure-cli.list
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+curl -L https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 sudo curl -o /etc/apt/sources.list.d/microsoft.list https://packages.microsoft.com/config/ubuntu/16.04/prod.list
 
+sudo apt-get install -y apt-transport-https
 sudo apt-get update
 
-sudo apt-get install -y apt-transport-https unzip docker.io python3 python-pip nodejs zsh
-sudo apt-get install -y --allow-unauthenticated powershell
+sudo apt-get install -y unzip docker.io python3 python-pip nodejs zsh powershell
+
 
 #sudo pip install --upgrade pip
 
