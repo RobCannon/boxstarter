1c4a88016e80e41fffdd7ffff8a95ebb01d91b44
Cannon, Robert <Robert.Cannon@turner.com>
2018-08-16 13:01:42 -0400

    Trying to fix node/npm install

--- boxstarter.sh
+++ boxstarter.sh
@@ -1,73 +1,124 @@
 # Set up symlinks to share files across computers
 #sudo chown $USER ~/.config
+echo ''
+echo '------'
+echo 'Smymlink for .profile'
 if [ -f ~/.profile ]; then
   rm ~/.profile
 fi
 ln -s /c/Users/$USER/OneDrive/Documents/Keep/Linux/.profile ~/.profile
+echo ''
 
+echo ''
+echo '------'
+echo 'Symlink for .bashrc '
 if [ -f ~/.bashrc ]; then
   rm ~/.bashrc
 fi
 ln -s /c/Users/$USER/OneDrive/Documents/Keep/Linux/.bashrc ~/.bashrc
+echo ''
 
+echo ''
+echo '------'
+echo 'Copying .shh keys'
 rm -rf ~/.ssh
 mkdir ~/.ssh
 cp /c/Users/$USER/OneDrive/Documents/Keep/Linux/.ssh/* ~/.ssh
 sudo chmod 700 ~/.ssh
 sudo chmod 600 ~/.ssh/id_rsa
 sudo chmod 600 ~/.ssh/id_rsa.pub
+echo ''
 
-# Prereqs for Microsoft tool installs
+echo ''
+echo '------'
+echo 'Installing docker'
 AZ_REPO=$(lsb_release -cs)
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | \
     sudo tee /etc/apt/sources.list.d/azure-cli.list
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 curl -L https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 sudo curl -o /etc/apt/sources.list.d/microsoft.list https://packages.microsoft.com/config/ubuntu/16.04/prod.list
+echo ''
 
+echo ''
+echo '------'
+echo 'Updating apt-get'
 sudo apt-get install -y apt-transport-https
 sudo apt-get update
+sudo apt-get install -y unzip
+echo ''
 
-sudo apt-get install -y unzip docker.io python3 python-pip nodejs zsh
+echo ''
+echo '------'
+echo 'Installing docker'
+sudo apt-get install -y docker.io
+echo ''
 
-# Install docker-compose
+echo ''
+echo '------'
+echo 'Installing python'
+sudo apt-get install -y python3 python-pip
+
+echo ''
+echo '------'
+echo 'Installing nodejs'
+sudo apt-get install -y nodejs
+sudo apt-get install -y build-essential
+sudo npm install -g npm
+echo ''
+
+echo ''
+echo '------'
+echo 'Installing docker-compose'
 sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+echo ''
 
-
-#sudo pip install --upgrade pip
-
-# Install kubectl
+echo ''
+echo '------'
+echo 'Installing kubectl'
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 mkdir ~/.kube & rm ~/.kube/config & ln -s /c/Users/$USER/OneDrive/Documents/Keep/Linux/.kube/config ~/.kube/config
 rm -rf ~/.kube/configs & ln -s /c/Users/$USER/OneDrive/Documents/Keep/Linux/.kube/configs ~/.kube/configs
+echo ''
 
-# Install Angular-cli
+echo ''
+echo '------'
+echo 'Installing @angular/cli'
 sudo npm install -g @angular/cli
+echo ''
 
-# Install aws client
+echo ''
+echo '------'
+echo 'Installing aws client'
 pip install awscli --user
 rm -rf ~/.aws & ln -s /c/Users/$USER/OneDrive/Documents/Keep/Linux/.aws ~/.aws
+echo ''
 
-# Install samld
-#pip install samlkeygen --user
-
-# Install terraform
+echo ''
+echo '------'
+echo 'Installing terraform'
 wget https://releases.hashicorp.com/terraform/0.11.7/terraform_0.11.7_linux_amd64.zip
 unzip terraform_0.11.7_linux_amd64.zip
 rm terraform_0.11.7_linux_amd64.zip
 sudo mv terraform /usr/local/bin/
+echo ''
 
-# Install azure-cli
+echo ''
+echo '------'
+echo 'Installing azure-cli'
 sudo apt-get install azure-cli
+echo ''
 
-
-# Install oh-my-zsh
+echo ''
+echo '------'
+echo 'Installing zsh'
 if [ -d ~/.oh-my-zsh ]; then
   sudo rm -rf ~/.oh-my-zsh
 fi
+sudo apt-get install -y zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 sudo git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 if [ -d ~/.zshrc ]; then
