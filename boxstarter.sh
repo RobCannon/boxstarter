14b02220322495ce4eaf5e7ec737faa1653972d1
Cannon, Robert <Robert.Cannon@turner.com>
2018-08-16 14:47:48 -0400

    Still fixing

--- boxstarter.sh
+++ boxstarter.sh
@@ -31,37 +31,20 @@ echo ''
 
 echo ''
 echo '------'
-echo 'Installing docker'
-AZ_REPO=$(lsb_release -cs)
-echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | \
-    sudo tee /etc/apt/sources.list.d/azure-cli.list
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-curl -L https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
-sudo curl -o /etc/apt/sources.list.d/microsoft.list https://packages.microsoft.com/config/ubuntu/16.04/prod.list
-echo ''
-
-echo ''
-echo '------'
-echo 'Updating apt-get'
+echo 'Setting package source and updating apt-get'
 sudo apt-get install -y apt-transport-https
-sudo apt-get update
 sudo apt-get install -y unzip
 echo ''
 
 echo ''
 echo '------'
-echo 'Installing docker'
-sudo apt-get install -y docker.io
-echo ''
-
-echo ''
-echo '------'
 echo 'Installing python'
-sudo apt-get install -y python3 python-pip
+sudo apt-get install -y curl python-software-properties python3 python-pip
 
 echo ''
 echo '------'
 echo 'Installing nodejs'
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt-get install -y build-essential
 sudo npm install -g npm
@@ -69,7 +52,8 @@ echo ''
 
 echo ''
 echo '------'
-echo 'Installing docker-compose'
+echo 'Installing docker'
+sudo apt-get install -y docker.io
 sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 echo ''
@@ -109,6 +93,12 @@ echo ''
 echo ''
 echo '------'
 echo 'Installing azure-cli'
+AZ_REPO=$(lsb_release -cs)
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | \
+    sudo tee /etc/apt/sources.list.d/azure-cli.list
+curl -L https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+sudo curl -o /etc/apt/sources.list.d/microsoft.list https://packages.microsoft.com/config/ubuntu/16.04/prod.list
+sudo apt-get update
 sudo apt-get install azure-cli
 echo ''
 
@@ -119,9 +109,22 @@ if [ -d ~/.oh-my-zsh ]; then
   sudo rm -rf ~/.oh-my-zsh
 fi
 sudo apt-get install -y zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-sudo git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
-if [ -d ~/.zshrc ]; then
-  sudo rm -rf ~/.zshrc
+if [ -f ~/.zshrc ]; then
+  sudo rm ~/.zshrc
 fi
 ln -s /c/Users/$USER/OneDrive/Documents/Keep/Linux/.zshrc ~/.zshrc
+echo ''
+
+echo ''
+echo '------'
+echo 'Upgrading packages'
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get autoremove -y
+echo ''
+
+echo ''
+echo '------'
+echo 'Installing oh-my-zsh'
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sudo git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
