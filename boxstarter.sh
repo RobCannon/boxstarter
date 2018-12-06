26a1410595777c998a86e85d5589fa267904001f
Cannon <Robert.Cannon@turner.com>
2018-12-06 06:47:00 -0500

    Updated .net core

--- boxstarter.sh
+++ boxstarter.sh
@@ -141,7 +141,7 @@ echo 'Installing dotnet'
 # sudo dpkg -i packages-microsoft-prod.deb
 # sudo apt-get install apt-transport-https
 # sudo apt-get update
-sudo apt-get install dotnet-sdk-2.1
+sudo apt-get install dotnet-sdk-2.2
 dotnet tool install --global dotnet-outdated
 echo ''
 
