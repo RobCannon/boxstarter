07b3a587f2dad66c452453ffc65a6111e69a479f
Robert Cannon <rob@cannonsoftware.com>
2019-05-24 08:31:12 -0400

    Upgrade to terraform 0.11.14

--- boxstarter.sh
+++ boxstarter.sh
@@ -115,9 +115,9 @@ echo ''
 echo ''
 echo '------'
 echo 'Installing terraform'
-wget https://releases.hashicorp.com/terraform/0.11.13/terraform_0.11.13_linux_amd64.zip
-unzip terraform_0.11.13_linux_amd64.zip
-rm terraform_0.11.13_linux_amd64.zip
+wget https://releases.hashicorp.com/terraform/0.11.14/terraform_0.11.14_linux_amd64.zip
+unzip terraform_0.11.14_linux_amd64.zip
+rm terraform_0.11.14_linux_amd64.zip
 sudo mv terraform /usr/local/bin/
 echo ''
 
