c039e966b86ddecd817bf906b545db1a3e45ff7e
Rob Cannon <rob@cannonsoftware.com>
2020-03-10 22:37:38 -0400

    Another attempt at font installation with admin

--- boxstarter.ps1
+++ boxstarter.ps1
@@ -180,111 +180,15 @@ code --install-extension visualstudioexptteam.vscodeintellicode
 npm install -g npm npm-check-updates rimraf typescript gulp @angular/cli 2>$null
 
 
-function Add-Font {
-    <#
-        #requires -Version 2.0
-        .SYNOPSIS
-        This will install Windows fonts.
-
-        .DESCRIPTION
-        Requries Administrative privileges. Will copy fonts to Windows Fonts folder and register them.
-
-        .PARAMETER Path
-        May be either the path to a font file to install or the path to a folder containing font files to install.
-        Valid file types are .fon, .fnt, .ttf,.ttc, .otf, .mmm, .pbf, and .pfm
-
-        .EXAMPLE
-        Add-Font -Path Value
-        Will get all font files from provided folder and install them in Windows.
-
-        .EXAMPLE
-        Add-Font -Path Value
-        Will install provided font in Windows.
-
-
-    #>
-
-
-
-    [CmdletBinding(DefaultParameterSetName = 'Directory')]
-    Param(
-        [Parameter(Mandatory = $false,
-            ValueFromPipeline = $True, ValueFromPipelineByPropertyName = $True)]
-        [Parameter(ParameterSetName = 'Directory')]
-        [ValidateScript( { Test-Path $_ -PathType Container })]
-        [System.String[]]
-        $Path,
-
-        [Parameter(Mandatory = $false,
-            ValueFromPipeline = $True, ValueFromPipelineByPropertyName = $True)]
-        [Parameter(ParameterSetName = 'File')]
-        [ValidateScript( { Test-Path $_ -PathType Leaf })]
-        [System.String]
-        $FontFile
-    )
-
-    begin {
-        Set-Variable Fonts -Value 0x14 -Option ReadOnly
-        $fontRegistryPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts"
-
-        $shell = New-Object -ComObject Shell.Application
-        $folder = $shell.NameSpace($Fonts)
-        $objfontFolder = $folder.self.Path
-        #$copyOptions = 20
-        $copyFlag = [string]::Format("{0:x}", 4 + 16)
-        $copyFlag
-    }
-
-    process {
-        switch ($PsCmdlet.ParameterSetName) {
-            "Directory" {
-                ForEach ($fontsFolder in $Path) {
-                    Write-Log -Info -Message "Processing folder {$fontsFolder}"
-                    $fontFiles = Get-ChildItem -Path $fontsFolder -File -Recurse -Include @("*.fon", "*.fnt", "*.ttf", "*.ttc", "*.otf", "*.mmm", "*.pbf", "*.pfm")
-                }
-            }
-            "File" {
-                $fontFiles = Get-ChildItem -Path $FontFile -Include @("*.fon", "*.fnt", "*.ttf", "*.ttc", "*.otf", "*.mmm", "*.pbf", "*.pfm")
-            }
-        }
-        if ($fontFiles) {
-            foreach ($item in $fontFiles) {
-                Write-Host "Processing font file {$item}"
-                if (Test-Path (Join-Path -Path $objfontFolder -ChildPath $item.Name)) {
-                    Write-Host "Font {$($item.Name)} already exists in {$objfontFolder}"
-                }
-                else {
-                    Write-Host "Font {$($item.Name)} does not exists in {$objfontFolder}"
-                    Write-Host "Reading font {$($item.Name)} full name"
-
-                    Add-Type -AssemblyName System.Drawing
-                    $objFontCollection = New-Object System.Drawing.Text.PrivateFontCollection
-                    $objFontCollection.AddFontFile($item.FullName)
-                    $FontName = $objFontCollection.Families.Name
-
-                    Write-Host "Font {$($item.Name)} full name is {$FontName}"
-                    Write-Host "Copying font file {$($item.Name)} to system Folder {$objfontFolder}"
-                    $folder.CopyHere($item.FullName, $copyFlag)
-
-                    $regTest = Get-ItemProperty -Path $fontRegistryPath -Name "*$FontName*" -ErrorAction SilentlyContinue
-                    if (-not ($regTest)) {
-                        New-ItemProperty -Name $FontName -Path $fontRegistryPath -PropertyType string -Value $item.Name
-                        Write-Host "Registering font {$($item.Name)} in registry with name {$FontName}"
-                    }
-                }
-            }
-        }
-    }
-    end {
-    }
-}
-
 Write-Host 'Install SauceCodePro font'
 $fontUrl = 'https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/SourceCodePro/Regular/complete/Sauce%20Code%20Pro%20Nerd%20Font%20Complete%20Windows%20Compatible.ttf'
 $fontFileName = 'Sauce Code Pro Nerd Font Complete Windows Compatible.ttf'
 $fontFilePath = "$env:TEMP\$fontFileName"
 Invoke-WebRequest $fontUrl -OutFile $fontFilePath -UseBasicParsing
-Add-Font -FontFile $fontFilePath
+if (-Not (Test-Path "C:\Windows\Fonts\$fontFileName")) {
+    $Destination = (New-Object -ComObject Shell.Application).Namespace(0x14)
+    $Destination.CopyHere($fontFilePath, 0x10)
+}
 Remove-Item $fontFilePath -Force
 
 # Ensure SSH config exists so it can be linked to WSL
