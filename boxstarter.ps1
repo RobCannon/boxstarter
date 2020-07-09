4f909c99c19bd6e83aa8249c20d459784a11a5bf
Rob Cannon <rob@cannonsoftware.com>
2020-07-08 22:50:42 -0400

    Updating to use winget

--- boxstarter.ps1
+++ boxstarter.ps1
@@ -29,17 +29,15 @@ Get-AppxPackage *Keeper* | Remove-AppxPackage
 Get-AppxPackage *Minecraft* | Remove-AppxPackage
 Get-AppxPackage *MarchofEmpires* | Remove-AppxPackage
 Get-AppxPackage *Plex* | Remove-AppxPackage
-Get-AppxPackage *Solitaire* | Remove-AppxPackage
 Get-AppxPackage *MixedReality* | Remove-AppxPackage
 Get-AppxPackage *Microsoft3D* | Remove-AppxPackage
 Get-AppxPackage *Print3D* | Remove-AppxPackage
-#Get-AppxPackage Windows.CBSPreview | Remove-AppxPackage
 Get-AppxPackage *LinkedIn | Remove-AppxPackage
 Get-AppxPackage *McAfeeSecurity | Remove-AppxPackage
 Get-AppxPackage Microsoft.Office.Desktop | Remove-AppxPackage
 Get-AppxPackage Microsoft.GetHelp | Remove-AppxPackage
 Get-AppxPackage Microsoft.BingWeather | Remove-AppxPackage
-Get-AppxPackage RivetNetworks.SmartByte | Remove-AppxPackage
+Get-AppxPackage Microsoft.BingWeather | Remove-AppxPackage
 
 Write-Host "Trusting PSGallery" -ForegroundColor Yellow
 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
@@ -47,6 +45,7 @@ Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 
 # Copy PowerShell profile
 if (-not (Test-Path "$env:USERPROFILE/Documents/PowerShell")) { New-Item "$env:USERPROFILE/Documents/PowerShell" -ItemType Directory | Out-Null }
+if (-not (Test-Path "$env:USERPROFILE/Documents/WindowsPowerShell")) { New-Item "$env:USERPROFILE/Documents/WindowsPowerShell" -ItemType Directory | Out-Null }
 Invoke-WebRequest https://github.com/RobCannon/boxstarter/raw/master/profiles/Powershell/Microsoft.PowerShell_profile.ps1 -OutFile "$env:USERPROFILE/Documents/PowerShell/Microsoft.PowerShell_profile.ps1"
 Invoke-WebRequest https://github.com/RobCannon/boxstarter/raw/master/profiles/Powershell/Microsoft.PowerShell_profile.ps1 -OutFile "$env:USERPROFILE/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1"
 
@@ -54,11 +53,6 @@ Invoke-WebRequest https://github.com/RobCannon/boxstarter/raw/master/profiles/Po
 # Install Powershell modules
 Write-Host "Installing PowerShell modules" -ForegroundColor Yellow
 Install-Module -Name ImportExcel -Scope CurrentUser
-Install-Module -Name SqlServer -Scope CurrentUser
-# Install-Module -Name AWSPowerShell -Scope CurrentUser
-# Install-Module -Name Azure -Scope CurrentUser
-Install-Module -Name Pester -Scope CurrentUser -Force -SkipPublisherCheck
-Install-Module -Name psake -Scope CurrentUser
 Install-Module -Name posh-git -Scope CurrentUser
 Install-Module -Name oh-my-posh -Scope CurrentUser
 
@@ -78,44 +72,45 @@ Set-ItemProperty -Path 'HKCU:\Console' -Name 'ScreenBufferSize' -Value 0x270f007
 Set-ItemProperty -Path 'HKCU:\Console' -Name 'WindowSize' -Value 0x240078 -Type DWord -Force
 Set-ItemProperty -Path 'HKCU:\Console' -Name 'QuickEdit' -Value 1 -Force
 
+winget install -e --id Microsoft.PowerShell
+winget install -e --id Git.Git
+winget install -e --id 7zip.7zip
+winget install -e --id Microsoft.WindowsTerminal
+winget install -e --id Microsoft.VisualStudioCode
+winget install -e --id Perforce.P4Merge
+winget install -e --id Valve.Steam
+Remove-ItemProperty HKCU:\Software\Microsoft\Windows\CurrentVersion\Run -Name 'Steam' -ErrorAction SilentlyContinue
 
-choco install -y microsoft-windows-terminal
-choco install -y vscode.portable
-choco install -y 7zip
+# # Install Scoop
+# Invoke-Expression (New-Object Net.WebClient).DownloadString('https://get.scoop.sh')
 
-# Install Scoop
-Invoke-Expression (New-Object Net.WebClient).DownloadString('https://get.scoop.sh')
-
-# Sccop required packages
-scoop install 7zip
-scoop install git
-scoop install gzip
+# # Sccop required packages
+# scoop install 7zip
+# scoop install git
+# scoop install gzip
 
 
 # Configure Git
-[environment]::setenvironmentvariable('GIT_SSH', (resolve-path (scoop which ssh)), 'USER')
+#[environment]::setenvironmentvariable('GIT_SSH', (resolve-path (scoop which ssh)), 'USER')
 git config --global credential.helper manager
 git config --global user.name "Rob Cannon"
 git config --global user.email "rob@cannonsoftware.com"
 git config --global core.autocrlf false
 
-scoop bucket add extras
+# scoop bucket add extras
 
-scoop install ssh-agent-wsl
+# scoop install ssh-agent-wsl
 
-# programming languages
-scoop install pwsh
-scoop install python
-scoop install diffmerge
-scoop install nodejs
+# # programming languages
+# scoop install python
+# scoop install nodejs
 
-# cloud and infrastructure tools
-scoop install terraform
-scoop install packer
-scoop install kubectl
-scoop install python
-scoop install azure-cli
-scoop install diffmerge
+# # cloud and infrastructure tools
+# scoop install terraform
+# scoop install packer
+# scoop install kubectl
+# scoop install azure-cli
+# scoop install diffmerge
 
 
 #--- Browsers ---
@@ -133,68 +128,74 @@ Get-ChildItem "$([Environment]::GetFolderPath('DesktopDirectory'))" | ? { $_.Nam
 #Get-ChildItem "$([Environment]::GetFolderPath('CommonDesktopDirectory'))" | ? { $_.Name -eq 'Acrobat Reader DC.lnk' } | Remove-Item
 
 
-code --install-extension abusaidm.html-snippets
-code --install-extension angular.ng-template
-code --install-extension austincummings.razor-plus
-code --install-extension christian-kohler.npm-intellisense
-code --install-extension christian-kohler.path-intellisense
-code --install-extension CoenraadS.bracket-pair-colorizer
-code --install-extension cssho.vscode-svgviewer
-code --install-extension davidbabel.vscode-simpler-icons
-code --install-extension deerawan.vscode-whitespacer
-code --install-extension DotJoshJohnson.xml
-code --install-extension DSKWRK.vscode-generate-getter-setter
-code --install-extension EditorConfig.EditorConfig
-code --install-extension eg2.tslint
-code --install-extension eg2.vscode-npm-script
-code --install-extension esbenp.prettier-vscode
-code --install-extension formulahendry.auto-close-tag
-code --install-extension formulahendry.auto-rename-tag
-code --install-extension hookyqr.beautify
-code --install-extension humao.rest-client
-code --install-extension IBM.output-colorizer
-code --install-extension jock.svg
-code --install-extension mauve.terraform
-code --install-extension mindginative.terraform-snippets
-code --install-extension moppitz.vscode-extension-auto-import
-code --install-extension ms-azure-devops.azure-pipelines
-code --install-extension ms-kubernetes-tools.vscode-kubernetes-tools
-code --install-extension ms-mssql.mssql
-code --install-extension ms-python.python
-code --install-extension ms-vscode.azure-account
-code --install-extension ms-vscode.csharp
-code --install-extension ms-vscode.PowerShell
-code --install-extension ms-vscode.typescript-javascript-grammar
-code --install-extension ms-vsts.team
-code --install-extension msjsdiag.debugger-for-chrome
-code --install-extension msjsdiag.debugger-for-edge
-code --install-extension natewallace.angular2-inline
-code --install-extension NoHomey.angular-io-code
-code --install-extension PeterJausovec.vscode-docker
-code --install-extension quicktype.quicktype
-code --install-extension redhat.vscode-yaml
-code --install-extension technosophos.vscode-helm
-code --install-extension tyriar.shell-launcher
-code --install-extension visualstudioexptteam.vscodeintellicode
+# code --install-extension abusaidm.html-snippets
+# code --install-extension angular.ng-template
+# code --install-extension austincummings.razor-plus
+# code --install-extension christian-kohler.npm-intellisense
+# code --install-extension christian-kohler.path-intellisense
+# code --install-extension CoenraadS.bracket-pair-colorizer
+# code --install-extension cssho.vscode-svgviewer
+# code --install-extension davidbabel.vscode-simpler-icons
+# code --install-extension deerawan.vscode-whitespacer
+# code --install-extension DotJoshJohnson.xml
+# code --install-extension DSKWRK.vscode-generate-getter-setter
+# code --install-extension EditorConfig.EditorConfig
+# code --install-extension eg2.tslint
+# code --install-extension eg2.vscode-npm-script
+# code --install-extension esbenp.prettier-vscode
+# code --install-extension formulahendry.auto-close-tag
+# code --install-extension formulahendry.auto-rename-tag
+# code --install-extension hookyqr.beautify
+# code --install-extension humao.rest-client
+# code --install-extension IBM.output-colorizer
+# code --install-extension jock.svg
+# code --install-extension mauve.terraform
+# code --install-extension mindginative.terraform-snippets
+# code --install-extension moppitz.vscode-extension-auto-import
+# code --install-extension ms-azure-devops.azure-pipelines
+# code --install-extension ms-kubernetes-tools.vscode-kubernetes-tools
+# code --install-extension ms-mssql.mssql
+# code --install-extension ms-python.python
+# code --install-extension ms-vscode.azure-account
+# code --install-extension ms-vscode.csharp
+# code --install-extension ms-vscode.PowerShell
+# code --install-extension ms-vscode.typescript-javascript-grammar
+# code --install-extension ms-vsts.team
+# code --install-extension msjsdiag.debugger-for-chrome
+# code --install-extension msjsdiag.debugger-for-edge
+# code --install-extension natewallace.angular2-inline
+# code --install-extension NoHomey.angular-io-code
+# code --install-extension PeterJausovec.vscode-docker
+# code --install-extension quicktype.quicktype
+# code --install-extension redhat.vscode-yaml
+# code --install-extension technosophos.vscode-helm
+# code --install-extension tyriar.shell-launcher
+# code --install-extension visualstudioexptteam.vscodeintellicode
 
 
-npm install -g npm npm-check-updates rimraf typescript gulp @angular/cli 2>$null
+#npm install -g npm npm-check-updates rimraf typescript gulp @angular/cli 2>$null
 
 
-Write-Host 'Install SauceCodePro font'
-$fontUri = [System.Uri]'https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/SourceCodePro/Regular/complete/Sauce%20Code%20Pro%20Nerd%20Font%20Complete%20Windows%20Compatible.ttf'
-$fontFileName = [System.Uri]::UnescapeDataString(($fontUri.Segments | Select -Last 1 ))
-$fontFilePath = "$env:TEMP\$fontFileName"
-Invoke-WebRequest $fontUri -OutFile $fontFilePath -UseBasicParsing
+Write-Host 'Install Developer Fonts'
+Install-UserFont -Uri 'https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/SourceCodePro/Regular/complete/Sauce%20Code%20Pro%20Nerd%20Font%20Complete%20Windows%20Compatible.ttf' \
+    -FontName 'SauceCodePro NF Regular'
+Install-UserFont -Uri 'https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/CascadiaCode/complete/Caskaydia%20Cove%20Regular%20Nerd%20Font%20Complete%20Windows%20Compatible.ttf' \
+    -FontName 'CaskaydiaCove NF Book'
 
-# Unable to figure out how to query personal fonts by file name
-# $fontFolder.items() will return a list of installed fonts, but the font name doens't match the font file name
-# Not sure how to extract the font name from the file
-#if (-Not (Test-Path "C:\Windows\Fonts\$fontFileName")) {
-$fontFolder = (New-Object -ComObject Shell.Application).Namespace(0x14)
-$fontFolder.CopyHere($fontFilePath, 0x10)
-#}
-Remove-Item $fontFilePath -Force
+# Write-Host 'Install SauceCodePro font'
+# $fontUri = [System.Uri]'https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/SourceCodePro/Regular/complete/Sauce%20Code%20Pro%20Nerd%20Font%20Complete%20Windows%20Compatible.ttf'
+# $fontFileName = [System.Uri]::UnescapeDataString(($fontUri.Segments | Select -Last 1 ))
+# $fontFilePath = "$env:TEMP\$fontFileName"
+# Invoke-WebRequest $fontUri -OutFile $fontFilePath -UseBasicParsing
+
+# # Unable to figure out how to query personal fonts by file name
+# # $fontFolder.items() will return a list of installed fonts, but the font name doens't match the font file name
+# # Not sure how to extract the font name from the file
+# #if (-Not (Test-Path "C:\Windows\Fonts\$fontFileName")) {
+# $fontFolder = (New-Object -ComObject Shell.Application).Namespace(0x14)
+# $fontFolder.CopyHere($fontFilePath, 0x10)
+# #}
+# Remove-Item $fontFilePath -Force
 
 # Ensure SSH config exists so it can be linked to WSL
 if (-Not (Test-Path $HOME\.ssh)) { New-Item $HOME\.ssh -ItemType Directory | Out-Null }
