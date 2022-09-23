199c19fe5df56fbfd08f74279cd91bc21f2589ca
Rob Cannon <rob@cannonsoftware.com>
2022-09-23 08:09:42 -0400

    Fixing Ubuntu installation

--- .local/bin/boxstarter.ps1
+++ .local/bin/boxstarter.ps1
@@ -92,6 +92,6 @@ $env:WSLENV = 'USERPROFILE/p:APPDATA/p:AWS_PROFILE'
 [environment]::setenvironmentvariable('WSLENV', $env:WSLENV, 'USER')
 $wsl_distributions = wsl --list
 if ($wsl_distributions -notcontains "Ubuntu-22.04" -or $wsl_distributions -notcontains "Ubuntu-22.04 (Default)") {
-  wsl --install -d Ubuntu-22.04
-  wsl --set-default Ubuntu-22.04
+  ubuntu2204.exe --ui=tui
 }
+wsl --set-default Ubuntu-22.04
